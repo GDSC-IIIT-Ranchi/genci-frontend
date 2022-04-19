6612ce79e6e8d435246cbc2422a74b9ed4262adf
Rahul917-Kumar <rahul66.ugcs20@iiitranchi.ac.in>
2022-04-19 20:10:27 +0530

    added routes and component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-
+import axios from "axios";
 function Form() {
   const { id } = useParams();
+  const url=""
+  axios.get(url)
+  .then((res)=>{
+     console.log(res)
+  }).catch(err=>console.log(err))
   console.log(id);
   return <></>;
 }
