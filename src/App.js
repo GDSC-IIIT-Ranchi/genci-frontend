c812a407d41431bc6ff7b3ac920760d774a188ba
abhishekrathi16 <rathiabhishek53@gmail.com>
2022-04-19 19:15:44 +0530

    initialised project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React,{ Component } from "react";
+import { BrowserRouter as Link, Router, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <div>
+      Hello world
+    </div>
+  );
+}
+
+export default App;
