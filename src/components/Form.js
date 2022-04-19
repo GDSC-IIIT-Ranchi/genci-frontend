f3a8a0de103d5e61a4ebd547ae04f4c12215c887
Rahul917-Kumar <rahul66.ugcs20@iiitranchi.ac.in>
2022-04-19 20:25:46 +0530

    added routes and component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 function Form() {
   const { id } = useParams();
   const url=""
+  const [allDetail, setDetail] = useState({});
   axios.get(url)
   .then((res)=>{
      console.log(res)
+     setDetail(...res.data)
   }).catch(err=>console.log(err))
   console.log(id);
-  return <></>;
+  return (
+    <>
+      <div>
+        <div>
+          <h2>{allDetail.name}</h2>
+          <h2>{allDetail.event_name}</h2>
+          <h2>{allDetail.date}</h2>
+        </div>
+        <div>
+          <img src={allDetail.certificate_url}></img>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Form;
