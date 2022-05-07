7a6f15d8b0fcf16863413b98836e69f277f13768
Rahul917-Kumar <rahul66.ugcs20@iiitranchi.ac.in>
2022-05-07 16:28:45 +0530

    added folders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Form from "./components/Form";
+import Show_certi from "./components/Show_certi";
 function App() {
   return (
     <>
       <Router>
         <Routes>
+          <Route path="/certi" element={<Show_certi/>}></Route>
           <Route path="/certi/:id" element={<Form />}></Route>
         </Routes>
       </Router>
---
+++ src/components/Show_certi.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import axios from 'axios';
+
+function Show_certi() {
+   const[id , setId] = useState()
+  
+  const check_availability = ()=>{
+      axios.post(url,{
+
+      })
+  }
+
+  return (
+    <div>
+      <form method="post">
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" value="id"  name="id" onChange={setId_input} />
+        <Button onclick={check_availability}>Submit</Button>
+      </form>
+    </div>
+  );
+}
+
+export default Show_certi
